aa6d6cc733492c3ba142b856c078d79c988e436b
qwerty34-qw <wh861440@gmail.com>
2020-11-13 23:25:46 +0200

    Add BusinessCard.dll

---
+++ BusinessCard/BusinessCard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneySpace;
+
+namespace BusinessCard
+{
+    public class BusinessCard
+    {
+        public string Company { get; set; }
+        public string Bank { get; set; }
+        public string Currency { get; set; }
+        public Money money;
+
+        public BusinessCard(string Company, string Bank, string Currency)
+        {
+            this.Company = Company;
+            this.Bank = Bank;
+            this.Currency = Currency;
+            money = new Money(Company + "_Card");
+        }
+    }
+}
